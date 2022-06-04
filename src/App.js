5ac47de7074c200ffd1851f302d9fb48e7a717b6
Iligiz <98862319+iligiz099@users.noreply.github.com>
2022-06-05 00:27:50 +0300

    Slider with Framer Motion

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
+
+function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <div className="App">
+       <motion.div ref={carousel} className="carousel" whileTap={{ cursor: "grabbing" }}>
+          <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className="inner-carousel">
+           {images.map((image) => {
+             return (
+               <motion.div className="item" key={image}>
+                  <img src={image} alt=""/>
+               </motion.div>
+             );
+           })}
+          </motion.div>
+       </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from "./assets/1.jpg";
+import image2 from "./assets/2.jpg";
+import image3 from "./assets/3.jpg";
+import image4 from "./assets/4.jpg";
+import image5 from "./assets/5.jpg";
+import image6 from "./assets/6.jpeg";
+
+export default [image1, image2, image3, image4, image5, image6];
